e80526074c43dcad289d643d9a9805560f2d861a
Ceendaa99 <cindakamilah0526@gmail.com>
2025-11-17 02:30:57 +0000

    ceenda

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-anjay mabar
+DataTransferItemList.prototype.add = function(data, type) {
+    const item = new DataTransferItem();
+    item.kind = 'string';
+    item.type = type || 'text/plain';
+    item.getAsString = function(callback) {
+        callback(data);
+    };
+    this.items.push(item);
+}                       
